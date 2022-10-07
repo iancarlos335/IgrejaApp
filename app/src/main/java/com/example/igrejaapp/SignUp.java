06844346559a05097aa96cfb174cd519a5f047bd
Ian Carlos Azevedo Martins <101537577+iancarlos335@users.noreply.github.com>
2022-10-06 23:32:00 -0300

    Começando a criar a classe que fará a pesquisa no banco

--- app/src/main/java/com/example/igrejaapp/SignUp.java
+++ app/src/main/java/com/example/igrejaapp/SignUp.java
@@ -2,13 +2,38 @@ package com.example.igrejaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
+
+    TextInputEditText textInputEditTextUsername, textInputEditTextPassword;
+    Button btnSignUp;
+    TextView txtLogin;  // TODO fazer um texto que redirecione o usuário para a página de login
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        textInputEditTextUsername = findViewById(R.id.username);
+        textInputEditTextPassword = findViewById(R.id.password);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() { //Dentro desse evento que serão precessados o cadastro dos dados (colocar as ações aqui).
+            @Override
+            public void onClick(View view) {
+
+               // new Task().execute(); // Nesse caso ele vai chamar uma classe pra fazer as ações
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/igrejaapp/connection/Task.java
@@ -0,0 +1,8 @@
+package com.example.igrejaapp.connection;
+
+import android.os.AsyncTask;
+
+public class Task extends AsyncTask<Void, Void, Void>{
+    String errors= "", result="";
+
+}
