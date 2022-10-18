69f5e3bbd736275cb5d37789f3b6c28f106dde45
Ian Carlos <iancarlos335@gmail.com>
2022-10-17 22:19:53 -0300

    Ultimas configurações de layout, e já iniciando as config de conexão php

--- app/src/main/java/com/example/igrejaapp/SignUp.java
+++ app/src/main/java/com/example/igrejaapp/SignUp.java
@@ -3,6 +3,7 @@ package com.example.igrejaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,51 +11,83 @@ import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.vishnusivadas.advanced_httpurlconnection.PutData; //eu acabo precisando do conteúdo git dele, o que pode ser um problema futuro
 
 public class SignUp extends AppCompatActivity {
 
-    TextInputEditText textInputEditTextUsername, textInputEditTextPassword;
+    TextInputEditText textInputEditTextFullname, textInputEditTextEmail,textInputEditTextUsername, textInputEditTextPassword ;
     Button btnSignUp;
-    TextView txtLogin;  // TODO fazer um texto que redirecione o usuário para a página de login
+    TextView textViewLogin;  // TODO fazer um texto que redirecione o usuário para a página de login
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        textInputEditTextFullname = findViewById(R.id.fullname);
+        textInputEditTextEmail = findViewById(R.id.email);
         textInputEditTextUsername = findViewById(R.id.username);
         textInputEditTextPassword = findViewById(R.id.password);
         btnSignUp = findViewById(R.id.btnSignUp);
+        textViewLogin = findViewById(R.id.loginText);
 
 
         //Dentro desse evento que serão precessados o cadastro dos dados (colocar as ações aqui).
-        btnSignUp.setOnClickListener(view -> {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullname, email, username, password;
+                fullname = String.valueOf(textInputEditTextFullname.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+                username = String.valueOf(textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
 
-            //Start ProgressBar first (Set visibility VISIBLE)
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(() -> {
-                //Starting Write and Read data with URL
-                //Creating array for parameters
-                String[] field = new String[2];
-                field[0] = "param-1";
-                field[1] = "param-2";
-                //Creating array for data
-                String[] data = new String[2];
-                data[0] = "data-1";
-                data[1] = "data-2";             //Fill that only with your own IPV4. Using CMD to consult
-                PutData putData = new PutData("http://192.168.0.196/ConnectionPhp/signup.php", "POST", field, data);
-                if (putData.startPut()) {
-                    if (putData.onComplete()) {
-                        String result = putData.getResult();
-                        //End ProgressBar (Set visibility to GONE)
-                    }
+
+                if (!fullname.equals("") && !email.equals("") && !username.equals("") && !password.equals("")) { // only if the fields are , the code will be executed
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "email";
+                            field[2] = "username";
+                            field[3] = "password";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = fullname;
+                            data[1] = email;
+                            data[2] = username;
+                            data[3] = password;
+                            PutData putData = new PutData("http://192.168.0.196/ConnectionPhp/signup.php", "POST", field, data); //Using the IP of machine where database is hosted, enable others devices to consult him only by being connected at same wifi
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Cadastro deu certo")){
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT ).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+
+                        }
+                    });
+
                 }
-                //End Write and Read data with URL
-            });
+                else {
+                    Toast.makeText(getApplicationContext(), "Todos os campos são obrigatórios", Toast.LENGTH_SHORT).show(); // Footer system message
+                }
 
+            }
         });
 
     }
